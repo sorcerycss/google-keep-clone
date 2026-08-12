6d5047c1f95311a124b86408e26533a331578573
sorcerycss <sorcerycss@gmail.com>
2026-08-11 20:01:37 -0700

    refactor form input/ close button fix

--- app.js
+++ app.js
@@ -11,6 +11,7 @@ class App {
         this.$noteTitle = document.querySelector('#note-title');
         this.$noteText = document.querySelector('#note-text');
         this.$formButtons = document.querySelector('#form-buttons');
+        this.$formCloseBtn = document.querySelector('#form-close-button');
 
         this.addEventListeners(); // runs when our app starts up
     }
@@ -30,13 +31,24 @@ class App {
                 this.addNote({ title, text});
             }
         });
+
+        this.$formCloseBtn.addEventListener('click', event => {
+            event.stopPropagation(); // to fix bubbling effect with handleFormClick function
+            this.closeForm();
+        });
     }
 
     handleFormClick(event) {
-      const isFormClicked = this.$form.contains(event.target); // returns true or false
+      const isFormClicked = this.$form.contains(event.target);
+
+      const title = this.$noteTitle.value;
+      const text = this.$noteText.value;
+      const hasNote = title || text;
 
       if (isFormClicked) {
         this.openForm();
+      } else if (hasNote) {
+        this.addNote({ title, text });
       } else {
         this.closeForm();
       }
@@ -56,10 +68,10 @@ class App {
         this.$noteText.value = '';
     }
 
-    addNote(note) {
+    addNote({ title, text }) {
         const newNote = {
-            title: note.title,
-            text: note.text,
+            title,
+            text,
             color: 'white',
             id: this.notes.length > 0 ?
                 this.notes[this.notes.length - 1].id + 1 : 1
