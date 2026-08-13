965f609a4c0fc677ecba51f33da315b987b622a3
sorcerycss <sorcerycss@gmail.com>
2026-08-13 11:31:27 -0700

    add ability to store note

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 class App {
     constructor() {
-        this.notes = [];
+        this.notes = JSON.parse(localStorage.getItem('notes')) || [];
         this.title = '';
         this.text = '';
         this.id = '';
@@ -19,6 +19,7 @@ class App {
         this.$modalCloseBtn = document.querySelector('.modal-close-button');
         this.$colorTooltip = document.querySelector('#color-tooltip');
 
+        this.render();
         this.addEventListeners();
     }
 
@@ -123,7 +124,7 @@ class App {
         this.id = event.target.dataset.id; // nextElementSibling alternative to data-id=${note.id}
         const noteCoords = event.target.getBoundingClientRect(); // specific info about coordinates
         const horizontal = noteCoords.left + window.scrollX; // determines how much user scrolled
-        const vertical = noteCoords.top + window.scrollY - 310;
+        const vertical = noteCoords.top + window.scrollY - 310; // custom solution - needs refactor
         this.$colorTooltip.style.transform = `translate(${horizontal}px, ${vertical}px)`;
         this.$colorTooltip.style.display = 'flex';
     }
@@ -142,7 +143,7 @@ class App {
                 this.notes[this.notes.length - 1].id + 1 : 1
         };
         this.notes = [...this.notes, newNote];
-        this.displayNotes();
+        this.render();
         this.closeForm();
     }
 
@@ -152,14 +153,14 @@ class App {
         this.notes = this.notes.map(note => 
             note.id === Number(this.id) ? { ...note, title, text } : note
         );
-        this.displayNotes();
+        this.render();
     }
 
     editNoteColor(color) {
         this.notes = this.notes.map(note =>
             note.id === Number(this.id) ? { ...note, color } : note
         );
-        this.displayNotes();
+        this.render();
     }
 
     selectNote() {
@@ -176,7 +177,16 @@ class App {
         if (!event.target.matches('.toolbar-delete')) return;
         const id = event.target.dataset.id;
         this.notes = this.notes.filter(note => note.id !== Number(id));
+        this.render();
+    }
+
+    render() {
+        this.saveNotes();
         this.displayNotes();
+    }
+
+    saveNotes() {
+        localStorage.setItem('notes', JSON.stringify(this.notes));
     }
 
     displayNotes() {
